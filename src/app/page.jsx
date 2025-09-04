f84cdf0f44f17532b0a048e5802d41655fa70327
Grasonyang <grasonjas@gmail.com>
2025-09-04 15:40:06 +0000

    init: V2

---
+++ src/app/page.jsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to the Secure Voting System
+        </h1>
+
+        <p className="mt-3 text-2xl">
+          Your vote matters.
+        </p>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <Link href="/vote" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+            <h3 className="text-2xl font-bold">Go to Voting Dashboard &rarr;</h3>
+            <p className="mt-4 text-xl">
+              View active elections and cast your vote.
+            </p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  )
+}
